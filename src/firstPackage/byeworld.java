36fa67c5da142d2474b8ff40a8e68ce85ea8afcd
joe jacobs <joehula@hotmail.com>
2022-08-23 11:31:28 -0400

    add byeworld.java

---
+++ src/firstPackage/byeworld.java
@@ -0,0 +1,8 @@
+package firstPackage;
+
+public class byeworld {
+    public static void main(String[] args) {
+        System.out.println("joe");
+        System.out.println("1.5 weeks of git");
+    }
+}
